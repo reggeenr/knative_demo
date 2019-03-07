e51dd39a5d1961e42a9d758fafaf7ce4329c212e
Jeremias Werner <jerewern@de.ibm.com>
2019-03-07 14:34:01 +0100

    Dockerfile and helloworld.go samples

---
+++ helloworld.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "os"
+    "strings"
+    "time"
+)
+
+func main() {
+    text := "Hello World Jeremias!"
+
+    rev := os.Getenv("K_REVISION") // K_REVISION=helloworld-s824d
+    if i := strings.LastIndex(rev, "-"); i > 0 {
+        rev = rev[i+1:]
+    }
+
+    msg := fmt.Sprintf("%s: %s\n", rev, text)
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Printf("Got request\n")
+        time.Sleep(500 * time.Millisecond)
+        fmt.Fprint(w, msg)
+    })
+
+    fmt.Printf("Listening on port 8080 (rev: %s)\n", rev)
+    http.ListenAndServe(":8080", nil)
+}
