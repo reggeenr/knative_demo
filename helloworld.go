ae1a38e48624f64b5d5517985816cfd013e3e2f6
Jeremias Werner <jerewern@de.ibm.com>
2019-03-12 11:13:33 +0100

    create an event

--- helloworld.go
+++ helloworld.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-    text := "Hello World Jeremias, it's demo time!"
+    text := "Hello World Jeremias, it's demo time now!"
 
     rev := os.Getenv("K_REVISION") // K_REVISION=helloworld-s824d
     if i := strings.LastIndex(rev, "-"); i > 0 {
