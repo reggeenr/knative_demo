b6e438bd7056240efc6cc679be017aefb4ef7bb3
Jeremias Werner <jerewern@de.ibm.com>
2019-03-22 19:13:00 +0100

    Update helloworld.go

--- helloworld.go
+++ helloworld.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-    text := "Hello World Jeremias, it's demo time 18:47 :-)"
+    text := "Hello World Jeremias, it's demo time 19:12 :-)"
 
     rev := os.Getenv("K_REVISION") // K_REVISION=helloworld-s824d
     if i := strings.LastIndex(rev, "-"); i > 0 {
